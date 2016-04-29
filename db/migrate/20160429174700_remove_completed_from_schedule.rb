1770d5e69ccf63cfdd3ac0223c7a2d0c9f36e046
Erin Obrock <erin.obrock@gmail.com>
2016-04-29 14:40:11 -0400

    testing

--- db/migrate/20160429174700_remove_completed_from_schedule.rb
+++ db/migrate/20160429174700_remove_completed_from_schedule.rb
@@ -1,5 +1,6 @@
 class RemoveCompletedFromSchedule < ActiveRecord::Migration
   def change
     remove_column :schedules, :completed, :boolean
+    change_column_null :schedules, :completed, false
   end
 end
