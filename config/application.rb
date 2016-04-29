ee4e2fc6e884e8edc936dbd8493d54b593e87aa2
Erin Obrock <erin.obrock@gmail.com>
2016-04-29 13:29:41 -0400

    Testing

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.serve_static_assets = true
 module VisualScheduler
 
   class Application < Rails::Application
