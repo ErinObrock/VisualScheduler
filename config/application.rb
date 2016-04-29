56eb410d7bb13c61ba2a46753eef0df17f028f34
Erin Obrock <erin.obrock@gmail.com>
2016-04-29 13:30:35 -0400

    Testing

--- config/application.rb
+++ config/application.rb
@@ -14,10 +14,11 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.serve_static_assets = true
+
 module VisualScheduler
 
   class Application < Rails::Application
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
